a49e8edac761dbe3e7c56d187533bfe042671e10
moransaka <moransaka15@gmail.com>
2018-10-04 21:56:37 +0300

    Pre_relise

--- Gas_Boiler_Control.ino
+++ Gas_Boiler_Control.ino
@@ -7,14 +7,14 @@ LiquidCrystal_I2C lcd(0x27, 16, 12);
 iarduino_RTC time(RTC_DS3231);
 
 // Создание пинов
-const short relay11 = 2;
-const short relay12 = 4;
-const short relay21 = 7;
+const short relay11 = 2;                   // Помпа
+const short relay12 = 4;                   // Газовый котёл
+const short relay21 = 7;                   // Электро котёл
 //const short relay22 = ;
 const short pin10 = 10;
 
 short times;                               // Переменные которые хранят время
-bool index;
+short index;
 
 void Display(bool i);
 void disabledAll();
@@ -22,6 +22,7 @@ void Tablet();
 void startClock();
 void initLCD();
 void relay(short i);
+void elBoiler5_7();
 
 void setup()
 {
@@ -64,42 +65,43 @@ void initLCD()
   lcd.init();                             // Инициализация дисплея  
   lcd.backlight();                        // Подключение подсветки
   lcd.setCursor(0, 0);                    // Установка курсора в начало первой строки
-  lcd.print("DONT PANIC!");               // Набор текста на второй строке
+  lcd.print("DONT PANIC!");               // Набор текста на первой строке
 }
 
-void disabledAll(){
+void disabledAll()
+{
   digitalWrite(relay21, HIGH);            // Отключить все пины
   digitalWrite(relay11, HIGH);
   digitalWrite(relay12, HIGH);
 }
 
-void relay(short i){                      // Управление реле
-  if (i > 5 && i < 10){
-    digitalWrite(relay21, HIGH);
-    index = true;  
-  }
-  else{
-    digitalWrite(relay21, LOW);
-  }
-  
-  if (i < 5 || i > 10){
+void relay(short i)
+{                      // Управление реле
+  if (i >= 7 && i <= 23){
     digitalWrite(relay12, HIGH);  
-    index = false;
+    digitalWrite(relay11, HIGH);
+    index = 2;
   }
   else{
     digitalWrite(relay12, LOW);
+    digitalWrite(relay11, LOW);
+    index = 0;
   }
   
-  if (i < 5 || i > 10){
+  if (i >= 23 && i <= 5){
     digitalWrite(relay11, HIGH);
-    index = false;
+    digitalWrite(relay21, HIGH);
+    index = 1;
   }
   else{
     digitalWrite(relay11, LOW);
+    digitalWrite(relay21, LOW);
+    index = 0;
   }
 }
 
-void Tablet(){                            // Если что-то сломаеться)))
+void Tablet()
+{                            // Если что-то сломаеться)))
   digitalWrite(relay12, HIGH);
   //Serial.print("yes");
   delay(500);
@@ -108,13 +110,33 @@ void Tablet(){                            // Если что-то сломает
   delay(500);
 }
 
-void Display(bool i){                     // Вывод на экран 
-  if (i == true){
-    lcd.setCursor(0,0);
-    lcd.print("GAS Boiler is works");
+void Display(short i)        // Вывод на экран
+{                      
+  
+  switch (i)
+  {
+    case 0:
+      lcd.setCursor(0,0);
+      lcd.print("DONT PANIC!");
+    case 1:
+      lcd.setCursor(0,0);
+      lcd.print("EL Boiler and Pomp");  
+    case 2:
+      lcd.setCursor(0,0);
+      lcd.print("GAS Boiler and Pomp");
+  }
+}
+
+void elBoiler5_7()
+{
+  if (i >= 5 && i <= 7){
+    digitalWrite(relay11, HIGH);
+    digitalWrite(relay21, HIGH);
+    index = 1;
   }
   else{
-    lcd.setCursor(0,0);
-    lcd.print("EL Boiler is works");
+    digitalWrite(relay11, LOW);
+    digitalWrite(relay21, LOW);
+    index = 0;
   }
 }
